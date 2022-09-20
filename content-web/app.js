2f4cfdcb993d271651108c710ffffb043460d0ee
jurij-robba <jurij.robba@protonmail.com>
2022-09-20 11:52:09 +0000

    Added Application Insights

--- content-web/app.js
+++ content-web/app.js
@@ -5,6 +5,10 @@ const request = require('request');
 
 const app = express();
 
+const appInsights = require("applicationinsights");
+appInsights.setup("f43c9c9b-8190-4da4-b823-0f0e51bc5a44");
+appInsights.start();
+
 app.use(express.static(path.join(__dirname, 'dist/content-web')));
 const contentApiUrl = process.env.CONTENT_API_URL || "http://localhost:3001";
 
